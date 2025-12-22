b33dab6c3193840202f034a8e7d88938c9acd291
deepanshu2001 <d.deepanshusharma12@gmail.com>
2025-12-21 16:33:50 -0800

    setup done for grok api

--- Backend/src/main/java/com/example/demo/DemoApplication.java
+++ Backend/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DemoApplication {
     @Bean
@@ -13,6 +15,8 @@ public class DemoApplication {
 		return new BCryptPasswordEncoder();
 	}
 	public static void main(String[] args) {
+		Dotenv dotenv=Dotenv.load();
+		System.setProperty("GROKGROK_API_KEY",dotenv.get("GROK_API_KEY"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
