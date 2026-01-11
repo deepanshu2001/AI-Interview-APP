89d31f1aa5115aaf2eceafa1b4bfec3d55b65fe8
deepanshu2001 <d.deepanshusharma12@gmail.com>
2026-01-10 16:00:15 -0800

    Fixed the securityConfig to handle cors error from netlify frontend

--- Backend/src/main/java/com/example/demo/Configuration/SecurityConfig.java
+++ Backend/src/main/java/com/example/demo/Configuration/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
-                "http://localhost:8080"
+                "http://localhost:8080",
+                "https://curious-caramel-ffc38a.netlify.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
